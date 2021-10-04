d5fbd98cc68260eed7c2cc96d2c239438b7b09cc
pjayathissa <p.jayathissa@gmail.com>
2021-10-04 21:02:35 +1300

    flowchart website up

---
+++ js/flowcharts.js
@@ -0,0 +1,30 @@
+function showContent() {
+  console.log('showing content')
+
+
+
+  for (var ii = 7; ii <30; ii++) {
+
+    var posterBoard = document.getElementById('poster-board')
+
+    var posterTemp = document.getElementById("poster-template");
+    var clon = posterTemp.content.cloneNode(true);
+    var image = clon.querySelector(".poster-image");
+    image.src='book/png/flowchart' + ii + '.png';
+    image.setAttribute( "onClick", "document.getElementById('flowchart-modal" + ii + "').style.display='block'" );
+    posterBoard.appendChild(clon);
+
+
+    var modalTemp = document.getElementById('modal-template')
+    var modalClone = modalTemp.content.cloneNode(true);
+    var modalImage = modalClone.querySelector('.modal-image')
+    modalImage.src = 'book/png/flowchart' + ii + '.png';
+
+    var modalDiv = modalClone.querySelector('.modal-div')
+    modalDiv.id = "flowchart-modal" + ii
+
+    posterBoard.appendChild(modalClone)
+  }
+
+
+}
